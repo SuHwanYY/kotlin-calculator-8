adf134de3e04f12993fa7e73276529591e948b96
tnghk <tnghks2100@naver.com>
2025-10-20 02:43:02 +0900

    feat: 사용자 입력 문자열을 Console.readLine()으로 받아오는 기능 구현

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -1,5 +1,11 @@
 package calculator
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    // TODO: 프로그램 구현
+    println("덧셈할 문자열을 입력해 주세요.")
+    val input = Console.readLine()
+
+    // 입력한 문자열이 적절하게 출력되는지 확인
+    println("입력받은 문자열: $input")
 }
